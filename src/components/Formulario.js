0c05e75fb4a07503e01be0cb1cf2622cf263f0d0
Victor <v.g.m_@hotmail.com>
2021-02-01 12:43:59 +0100

    Tercer commit

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from "react";
 import {v4 as uuidv4} from 'uuid';
 
-const Formulario = () => {
+const Formulario = ({crearCita}) => {
 
     // Crear State de Citas
     const [cita, actualizarCita] = useState({
@@ -40,11 +40,19 @@ const Formulario = () => {
 
       // Asignar un ID
       cita.id = uuidv4();
-      console.log(cita);
+      
 
       // Crear la cita
+      crearCita(cita);
 
       // Reiniciar el form
+      actualizarCita({
+        mascota: '',
+        propietario:'',
+        fecha:'',
+        hora:'',
+        sintomas:''
+      })
 
 
     }
