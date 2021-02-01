33cdea3a3effd8487db48a6f9bbc13e6a39526c4
Victor <v.g.m_@hotmail.com>
2021-02-01 17:47:15 +0100

    Ultimo commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
 import React, { Fragment, useState, useEffect } from "react";
-import Formulario from './components/Formulario';
-import Cita from './components/Cita';
+import Formulario from "./components/Formulario";
+import Cita from "./components/Cita";
+import PropTypes from 'prop-types';
 
 function App() {
-
+  // Citas en local storage
+  let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+  if (!citasIniciales) {
+    citasIniciales = [];
+  }
   // Arreglo de citas
-  const [citas, guardarCitas] = useState([]);
+  const [citas, guardarCitas] = useState(citasIniciales);
 
   // Use Effect para realizar ciertas operaciones cuando el state cambia
-  useEffect( () => {
-
-  }, [citas] );
+  useEffect(() => {
+    let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+    if (citasIniciales) {
+      localStorage.setItem("citas", JSON.stringify(citas));
+    } else {
+      localStorage.setItem("citas", JSON.stringify([]));
+    }
+  }, [citas]);
 
   // Funcion que tome las citas actuales y agregue la nueva
-  const crearCita = cita => {
-    guardarCitas([
-      ...citas,
-      cita
-    ])
-  }
+  const crearCita = (cita) => {
+    guardarCitas([...citas, cita]);
+  };
 
   // Funcion que elimina una cita por su id
 
-  const eliminarCita = id => {
-    const nuevasCitas = citas.filter(cita => cita.id !== id);
+  const eliminarCita = (id) => {
+    const nuevasCitas = citas.filter((cita) => cita.id !== id);
     guardarCitas(nuevasCitas);
-  }
+  };
 
   // Mensaje condicional
-    const titulo = citas.length === 0 ? "No hay citas" : "Administra tus citas";
+  const titulo = citas.length === 0 ? "No hay citas" : "Administra tus citas";
 
   return (
     <Fragment>
@@ -37,24 +44,22 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="one-half column">
-            <Formulario 
-              crearCita={crearCita}
-            />
+            <Formulario crearCita={crearCita} />
           </div>
           <div className="one-half column">
-          <h2>{ titulo }</h2>
-          {citas.map(cita => (
-            <Cita
-              key={cita.id}
-              cita={cita}
-              eliminarCita={eliminarCita}
-            />
-          ))}
+            <h2>{titulo}</h2>
+            {citas.map((cita) => (
+              <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
+            ))}
           </div>
         </div>
       </div>
     </Fragment>
   );
+}
+
+Formulario.propTypes = {
+  crearCita: PropTypes.func.isRequired
 }
 
 export default App;
